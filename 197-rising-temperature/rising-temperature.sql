60692a0e9c1aa2884b7d55798aafb8bae3dac458
BitsbyBhargav <officialbhargav22@gmail.com>
2026-05-11 12:53:03 +0530

    Time: 543 ms (22.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select today.id
+from Weather yesterday
+cross join Weather today
+where datediff(day,yesterday.recordDate,today.recordDate)=1 and today.temperature>yesterday.temperature
+;
+
+
