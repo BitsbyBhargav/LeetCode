026f5b0477c35adb1d9b47bca348d028ca95fbdb
BitsbyBhargav <officialbhargav22@gmail.com>
2026-05-13 18:08:38 +0530

    Time: 1079 ms (93.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,4 @@
+-- Write your SQL query statement below
+select p.project_id, round(1.0*sum(e.experience_years)/count(e.employee_id),2) as average_years
+from Project p join Employee e on p.employee_id=e.employee_id
+group by project_id
