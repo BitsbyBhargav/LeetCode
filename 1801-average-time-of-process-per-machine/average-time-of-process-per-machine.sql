62fbcbe3bdfb26e199baabc1b824e1eafa2246ab
Bhargav Sonawane <officialbhargav22@gmail.com>
2026-06-12 15:26:43 +0530

    Time: 253 ms (87.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+
+SELECT machine_id, ROUND(AVG(timestamp),3) as processing_time
+FROM (
+    SELECT machine_id, MAX(timestamp)-MIN(timestamp) as timestamp
+    FROM Activity
+    GROUP BY process_id,machine_id
+) as u
+GROUP BY machine_id;
